8a8724c266ae8441c0e8d0ad33a98847e92d9cfa
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-04-29 03:09:12 +0200

    Test file

---
+++ frontend/test.php
@@ -0,0 +1,9 @@
+<?php
+$_CVM = true;
+require("includes/include.base.php");
+
+$ssh = new SshConnector();
+$ssh->host = "cvm-vz.cryto.net";
+$ssh->key = "/etc/cvm/key";
+$ssh->pubkey = "/etc/cvm/key.pub";
+?>
