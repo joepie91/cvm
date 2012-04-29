8bed54a4eb2bc3c0649b6c83393343aaa1210885
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-04-29 16:05:45 +0200

    Testing stuff

--- frontend/test.php
+++ frontend/test.php
@@ -17,13 +17,16 @@ elseif($_GET['action'] == "stop")
 }
 elseif($_GET['action'] == "bw")
 {
-	$result = $sContainer->sNode->ssh->RunCommand("vzctl exec {$sContainer->sInternalId} cat /proc/net/dev", false);
-	$values = split_whitespace($result->stdout);
-	pretty_dump($values[1], $values[9]);
+	$result = $sContainer->sNode->ssh->RunCommand("vzctl exec {$sContainer->sInternalId} cat /proc/net/dev | grep venet0", false);
+	$lines = split_lines($result->stdout);
+	
+	$values = split_whitespace($lines[0]);
+	pretty_dump($values);
 }
 
 echo("Done!");
 ?>
+<title>CVM test</title>
 <br><br>
 <a href="?action=start">Start</a><br>
 <a href="?action=stop">Stop</a><br>
