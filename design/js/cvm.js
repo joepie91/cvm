2c7fad37c0c2744ce92aedcbe87e88858ee2a110
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-04-29 23:06:24 +0200

    Add loading icon

---
+++ design/js/cvm.js
@@ -0,0 +1,17 @@
+var command_running = false;
+
+$(function(){
+	$('.button-loader').click(function(){
+		if(command_running === false)
+		{
+			$('.button-loader').addClass('disabled').click(function(event){
+				event.preventDefault();
+				event.stopPropagation();
+				return false;
+			});
+			
+			$(this).children('.button-icon').addClass('running')[0].src = "images/loading.gif";
+			command_running = true;
+		}
+	});
+});
