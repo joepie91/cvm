8b854b73850de1bd97afa1f776cfeaf9c5e7d236
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-04-29 10:06:13 +0200

    Improved spacing in pretty_dump

--- frontend/cphp/include.misc.php
+++ frontend/cphp/include.misc.php
@@ -98,10 +98,19 @@ function clean_array($arr)
 function pretty_dump($input)
 {
 	ob_start();
+	
 	var_dump($input);
+	
 	$output = ob_get_contents();
 	ob_end_clean();
-	$output = nl2br(str_replace(" ", "&nbsp;&nbsp;&nbsp;", $output));
+	
+	while(preg_match("/^[ ]*[ ]/m", $output) == 1)
+	{
+		$output = preg_replace("/^([ ]*)[ ]/m", "$1&nbsp;&nbsp;&nbsp;", $output);
+	}
+	
+	$output = nl2br($output);
+	
 	echo($output);
 }
 
