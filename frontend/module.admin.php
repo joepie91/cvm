df4fe46ca80846087e00807eeb76ac7dff92b59f
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-05-11 01:48:02 +0200

    Add some stuff to admin panel

---
+++ frontend/module.admin.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * CVM is more free software. It is licensed under the WTFPL, which
+ * allows you to do pretty much anything with it, without having to
+ * ask permission. Commercial use is allowed, and no attribution is
+ * required. We do politely request that you share your modifications
+ * to benefit other developers, but you are under no enforced
+ * obligation to do so :)
+ * 
+ * Please read the accompanying LICENSE document for the full WTFPL
+ * licensing text.
+ */
+
+if(!isset($_CVM)) { die("Unauthorized."); }
+
+try
+{
+	$sUser->RequireAccessLevel(20);
+	
+	$sError = "";
+	$sPageContents = "";
+
+	$sMainClass = "shift";
+
+	$router = new CPHPRouter();
+	
+	$router->ignore_query = true;
+
+	$router->routes = array(
+		0 => array(
+			'^/admin/?$'			=> "module.admin.overview.php"
+		)
+	);
+
+	$router->RouteRequest();
+
+	$sMainContents .= Templater::InlineRender("main.admin", $locale->strings, array(
+		'contents'		=> $sPageContents
+	));
+}
+catch (InsufficientAccessLevelException $e)
+{
+	$err = new CPHPErrorHandler(CPHP_ERRORHANDLER_TYPE_ERROR, "You are not authorized to view this page", "Your access level is not sufficient.");
+	$sMainContents .= $err->Render();
+}
+?>
---
+++ frontend/templates/main.admin.tpl
@@ -0,0 +1,7 @@
+<div class="sidebar">
+	<a class="sidebutton" href="/admin/"><%!menu-admin-overview></a>
+	<a class="sidebutton" href="/admin/users/"><%!menu-admin-users></a>
+	<a class="sidebutton" href="/admin/containers/"><%!menu-admin-containers></a>
+</div>
+
+<%?contents>
